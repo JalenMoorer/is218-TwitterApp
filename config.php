d6491e243bbb3328ad94ef90b75bd1ef37c8cbd6
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-10-26 16:09:28 -0400

    Using Abraham's Twitter API Library to receive calls

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'Pqh0FejmsnCcpX9aiamMzhmVc');
+define('CONSUMER_SECRET', 'Gcy9DNEzARrK4cOMMsSq7T0v9JopBW0bZeyd9vP7i75XEPsWjA');
+define('OAUTH_CALLBACK', 'http://web.njit.edu/~jmm77/is218/Twitter2/callback.php');
